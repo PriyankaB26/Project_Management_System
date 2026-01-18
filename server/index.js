54d74df4de5c8992f9b22f23f6ee24ac4c3e795b
PriyankaB26 <145274707+PriyankaB26@users.noreply.github.com>
2026-01-19 00:43:33 +0530

    url1

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/activities", activityRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.FRONTEND_URL || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
 });
