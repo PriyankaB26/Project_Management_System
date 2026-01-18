e141b1612eda684664fa862851ee17e1f01f5f35
PriyankaB26 <145274707+PriyankaB26@users.noreply.github.com>
2026-01-19 03:18:22 +0530

    redep

--- server/services/projectService.js
+++ server/services/projectService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://project-management-system-zf4d.onrender.com", 
+  baseURL: "https://project-management-system-l1at.onrender.com", 
 });
 
 API.interceptors.request.use((req) => {
